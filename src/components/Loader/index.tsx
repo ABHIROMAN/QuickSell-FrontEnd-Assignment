0f650c7df48ea1f7d17a332662307d18ff26924b
Abhishek Kumar <72148582+ABHIROMAN@users.noreply.github.com>
2023-12-09 14:38:15 +0530

    Add files via upload

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './loader.css'
+
+function Loader({ fullscreen = true }: { fullscreen?: boolean }) {
+    return (
+        <div className={`loader-container ${fullscreen && "fullscreen"}`}>
+            <span className='loader'>Loading...</span>
+        </div>
+    );
+}
+
+export default Loader;
+// Designed by Abhishek
